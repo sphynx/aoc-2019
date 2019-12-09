fb9cd6ea68f8e358f64e2a51e9536dbd9211645c
Ivan Veselov <veselov@gmail.com>
2019-12-09 16:17:15 +0000

    Move solutions for each day into separate tests

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn solutions_test() {
+    fn day1_solution() {
         assert_eq!(day1::solve_part1(), 3296269);
         assert_eq!(day1::solve_part2(), 4941547);
+    }
+
+    #[test]
+    fn day2_solution() {
         assert_eq!(day2::solve_part1(), 3166704);
         assert_eq!(day2::solve_part2(), 8018);
+    }
+
+    #[test]
+    fn day3_solution() {
         assert_eq!(day3::solve_part1(), 855);
         assert_eq!(day3::solve_part2(), 11238);
+    }
+
+    #[test]
+    #[ignore] // It's too long!
+    fn day4_solution() {
         assert_eq!(day4::solve_part1(), 1764);
         assert_eq!(day4::solve_part2(), 1196);
+    }
+
+    #[test]
+    fn day5_solution() {
         assert_eq!(day5::solve_part1(), 15097178);
         assert_eq!(day5::solve_part2(), 1558663);
+    }
+
+    #[test]
+    fn day6_solution() {
         assert_eq!(day6::solve_part1(), 223251);
         assert_eq!(day6::solve_part2(), 430);
-
     }
 }
